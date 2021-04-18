6e8fe87e1cc3b59652baae5b69abf9fee73c99a3
And <and.stuber@gmail.com>
2021-04-18 19:15:43 -0300

    Lint fix: Linha 22: Unnecessary use of boolean.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 const port = process.env.PORT;
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true' ? true : false;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
